a6a2d5e70009e4618330c44af921bd87e2857f8b
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-09-25 14:51:32 +0600

    Get Features Data

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DEVSPOTLIGHT_USER_NAME}:${process.env.DEVSPOTLIGHT_PASSWORD}@cluster0.2xcsswz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,6 +26,14 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const featuresCollection = client.db("devspotDB").collection("features");
+
+    // Get Features Data
+    app.get("/api/v1/features", async (req, res) => {
+      const result = await featuresCollection.find().toArray();
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
