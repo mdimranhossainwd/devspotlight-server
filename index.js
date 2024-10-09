cc04a9b170bfa01c36e878fd580116b50a3bb5ba
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-10-09 10:15:51 +0600

    Cors origin changed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: [
-    "devspotlight-9f31a.web.app",
+    "https://devspotlight-9f31a.web.app",
     "http://localhost:5173",
     "http://localhost:5174",
   ],
@@ -343,12 +343,12 @@ async function run() {
     });
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
